ed5f2f711162f9caa6a831d1ccafaf8b47e5aa09
allenpbinoy <62869966+allenpbinoy@users.noreply.github.com>
2022-07-21 07:44:02 +0530

    40

--- lib/mobileNo.dart
+++ lib/mobileNo.dart
@@ -49,13 +49,6 @@ class _MobileNoState extends State<MobileNo> {
 
     print(response.body);
     var body = response.body;
-
-    if (response.body != null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => TabScreen()),
-      );
-    }
   }
 
   Future<void> send() async {
